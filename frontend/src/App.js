18d74da347c075d582cc8a4f470f5215f46f07d0
Abed <abedmugambi@gmail.com>
2024-02-29 23:25:40 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      
+       <h1>heyyyyyy</h1>
+       {/* git config --global user.email */}
+       {/* git config --global user.name "Your Name" */}
+    </div>
+  );
+}
+
+export default App;
